8019d65c0ce04580b10ab9cd140f1540d3c192c0
yagoubigithub <yagoubi.aek.2@gmail.com>
2019-10-27 14:04:35 +0100

    add carousel

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+const responsive = {
+    0:{
+        items :1
+    },
+    320:{
+        items:1
+    },
+    560:{
+        items:2
+    },
+    960:{
+        items : 3
+
+    }
+}
 $(document).ready(function(){
     $nav = $('.nav');
     $toggleCollapse = $('.toggle-collapse')
@@ -8,5 +23,17 @@ $(document).ready(function(){
     $toggleCollapse.click(function(){
         $nav.toggleClass('collapse');
     })
+
+      // owl-carousel for blog
+
+      $('.owl-carousel').owlCarousel({
+        loop :true,
+        autoplay :false,
+        autoplayTimeout: 3000,
+        dots : false,
+        nav :true,
+        navText : [$('.owl-navigation  .owl-nav-prev'),$('.owl-navigation  .owl-nav-next')],
+        responsive :responsive
+    });
 
 });
